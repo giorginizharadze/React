8fcc7e27f8ecf4faa14d6140245d8ab4bde72a59
giorginizharadze <giorgi.nizharadze99@gmail.com>
2022-01-09 20:41:28 +0400

    pokemon app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import PokemonThumbnail from './components/PokemonThumbnail';
 
 function App() {
+
+    const [allPokemons, setAllPokemons] = useState([])
+    const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+    const getAllPokemons = async () => {
+        const res = await fetch(loadMore)
+        const data = await res.json()
+
+        setLoadMore(data.next)
+
+        function createPokemonObject(result) {
+            result.forEach(async (pokemon) => {
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+                const data = await res.json()
+
+                setAllPokemons(currentList => [...currentList, data])
+
+                await console.log(allPokemons)
+            });
+        }
+
+        createPokemonObject(data.results)
+    }
+
+    useEffect(() => {
+        getAllPokemons()
+    }, [])
   return (
+    // <div className="app-container">
     <div className="App">
-      <h1> Hello React </h1>
+      <h1> Pokemon Evolution </h1>
+      <div className='pokemon-container'>
+          <div className='all-containers'>
+            {allPokemons.map((pokemon, index) => 
+                <PokemonThumbnail
+                id={pokemon.id}
+                name={pokemon.name}
+                image={pokemon.sprites.other.dream_world.front_default}
+                type={pokemon.types[0].type.name}
+                key={index}
+                />
+                )}
+          </div>
+          <button className='load-more' onClick={() => getAllPokemons()}> Load More </button>
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
